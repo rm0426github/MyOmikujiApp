a9467c19ccc07af7910582c51335aae7e9a3f7f8
rm0426 <rm0426@gmail.com>
2016-02-02 00:46:56 +0900

    数字に紐づく結果を定義 結果を色づけて表示

--- app/src/main/java/com/example/rm0426/myomikujiapp/MainActivity.java
+++ app/src/main/java/com/example/rm0426/myomikujiapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rm0426.myomikujiapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,22 +10,38 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String[] results = {
+            "大吉",
+            "吉",
+            "凶"
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    // TODO setOnClickListenerを使っても書いてみる.
     public void getOmikuji(View view) {
         // TextViewの取得.
         TextView txtView = (TextView) findViewById(R.id.myTextView);
 
         // 乱数の生成.
         Random randomGenerator = new Random();
-        int num = randomGenerator.nextInt(3);
+        int num = randomGenerator.nextInt(results.length);
 
         // 結果の表示.
-        String result = Integer.toString(num);
+        String result = results[num];
         txtView.setText(result);
+
+        // 大吉の時は赤、それ以外は黒にする.
+        if(num == 0) {
+            txtView.setTextColor(Color.RED);
+        }
+        else {
+            txtView.setTextColor(Color.rgb(0, 0, 0));
+        }
     }
 }
